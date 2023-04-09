144c470ce56427950fd56434111469c2e736e299
Ming <455747495@qq.com>
2023-04-09 22:42:41 +0800

    init

---
+++ prism_practise/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using prism_practise.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace prism_practise
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+    }
+}
---
+++ prism_practise/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using prism_practise.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prism_practise.ViewModels
+{
+    public class MainViewModel:BindableBase
+    {
+        public DelegateCommand<string> OpenCommand { get; private set; }
+
+
+
+
+        public MainViewModel()
+        {
+            OpenCommand = new DelegateCommand<string>(Open);
+        }
+
+        private object body;
+
+        public object Body
+        {
+            get { return body; }
+            set { body = value; RaisePropertyChanged(); }
+        }
+
+
+
+        private void Open (string obj)
+        {
+            switch (obj) {
+                case "ViewA": Body = new ViewA(); break;
+                case "ViewB": Body = new ViewB(); break;
+                case "ViewC": Body = new ViewC(); break;
+
+            }
+        }
+    }
+}
